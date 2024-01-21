22d39503c9dbf70cb981eedf6e6df6cb8f757d39
supratik <supratikpal53@gmail.com>
2024-01-21 16:24:54 +0530

    homepage updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
       <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center px-4 lg:px-0 flex-1'>
         <h1 className='text-4xl lg:text-[58px] font-semibold mb-6 leading-none'><span className='text-violet-700'>Rent</span> Your Dream House with us</h1>
         <p className='max-w-[480px] mb-8'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, pariatur?
+          A Seamless Experience in Finding Houses for Sale or Rent
         </p>
       </div>
       {/* image */}
